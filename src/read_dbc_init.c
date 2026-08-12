9de9a3434ab6ec4e2f1b8bc0807fa61c695bdde5
Anttony35 <93285554+Anttony35@users.noreply.github.com>
2026-08-12 12:54:12 -0300

    Update read_dbc_init.c

--- src/read_dbc_init.c
+++ src/read_dbc_init.c
@@ -3,18 +3,24 @@
 #include <stdlib.h> // for NULL
 #include <R_ext/Rdynload.h>
 
-/* FIXME: 
-   Check these declarations against the C/Fortran source code.
-*/
-
 /* .C calls */
 extern void dbc2dbf(void *, void *, void *, void *);
+
+extern void dbc2csv_select(
+    void *,
+    void *,
+    void *,
+    void *,
+    void *,
+    void *
+);
 
 /* .Call calls */
 extern SEXP dbf2dbc(SEXP, SEXP);
 
 static const R_CMethodDef CEntries[] = {
-    {"dbc2dbf", (DL_FUNC) &dbc2dbf, 4},
+    {"dbc2dbf",        (DL_FUNC) &dbc2dbf,        4},
+    {"dbc2csv_select", (DL_FUNC) &dbc2csv_select, 6},
     {NULL, NULL, 0}
 };
 
@@ -25,6 +31,13 @@ static const R_CallMethodDef CallEntries[] = {
 
 void R_init_read_dbc(DllInfo *dll)
 {
-    R_registerRoutines(dll, CEntries, CallEntries, NULL, NULL);
+    R_registerRoutines(
+        dll,
+        CEntries,
+        CallEntries,
+        NULL,
+        NULL
+    );
+
     R_useDynamicSymbols(dll, FALSE);
 }
